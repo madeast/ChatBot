f3206471a26cc30edec5cce5ba98650252782702
Easton Madsen <eeagle25@comcast.net>
2015-10-21 08:31:57 -0600

    Creating a ChatBot.

---
+++ src/chat/controller/ChatRunner.java
@@ -0,0 +1,15 @@
+package chat.controller;
+/**
+ * This runs the project.
+ * @author emad6932
+ * @version 1.0 10/21/15
+ * 
+ */
+public class ChatRunner
+{
+	public static void main (String[] args)
+	{
+	chatController mycontroller = new chatController();
+	mycontroller.start();
+	}
+}
---
+++ src/chat/controller/chatController.java
@@ -0,0 +1,15 @@
+package chat.controller;
+
+/**
+ * Application controller for the Chatbot project.
+ * @author Easton Madsen
+ * @version 1.0 10/21/15
+ *
+ */
+public class chatController
+{
+	public void start()
+	{
+		
+	}
+}
