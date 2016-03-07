dce5fc8f9b298baa208e5fec9135a1b578608661
Easton Madsen <eeagle25@comcast.net>
2016-03-07 09:15:07 -0700

    Added methods, and Declarations

--- src/chat/model/CtecTwitter.java
+++ src/chat/model/CtecTwitter.java
@@ -1,6 +1,31 @@
 package chat.model;
 
+import twitter4j.*;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Easton Madsen
+ * @version .1
+ * 
+ *
+ */
+
 public class CtecTwitter
 {
-
+	//Declarations
+	private ArrayList<Status> statusList;
+	private ArrayList<String> wordList;
+	private Twitter chatBotTwitter;
+	
+	public CtecTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>();
+	}
+	
+	public void sendTweet(String tweet)
+	{
+		
+	}
 }
